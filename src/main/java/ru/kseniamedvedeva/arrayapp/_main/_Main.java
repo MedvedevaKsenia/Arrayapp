6257cd72eaa18e5fb4f20ff53f7a9222294c87cb
Ksenia <ksenia.medvedeva.1993@gmail.com>
2024-03-09 16:55:06 +0500

    feat: created methods

--- src/main/java/ru/kseniamedvedeva/arrayapp/_main/_Main.java
+++ src/main/java/ru/kseniamedvedeva/arrayapp/_main/_Main.java
@@ -1,11 +1,28 @@
 package ru.kseniamedvedeva.arrayapp._main;
 
+import static ru.kseniamedvedeva.arrayapp.service.ArrayFunctions.*;
+
 public class _Main {
     public static void main(String[] args) {
         int[] intArray = new int[10];
+        System.out.println("Our array:");
         for (int i = 0; i < intArray.length; i++) {
-            intArray[i] = (int) (Math.random() * 100);
-            System.out.println(intArray[i]);
+            intArray[i] = (int) (Math.random() * 10);
+            System.out.print(intArray[i] + ", ");
         }
+        System.out.println();
+
+        int countEvenNumb = CountEvenNumbersInArray(intArray);
+        System.out.println(countEvenNumb + " numbers are even in the array");
+
+        int countOddNumb = CountOddNumbersInArray(intArray);
+        System.out.println(countOddNumb + " numbers are odd in the array");
+
+        int sum = SumNumbersInArray(intArray);
+        System.out.println("Summa all numbers in the array = " + sum);
+
+        int zeroNum = FindZeroInArray(intArray);
+        System.out.println(zeroNum + " zero in the array");
+
     }
 }
--- src/main/java/ru/kseniamedvedeva/arrayapp/service/ArrayFunctions.java
+++ src/main/java/ru/kseniamedvedeva/arrayapp/service/ArrayFunctions.java
@@ -2,5 +2,41 @@ package ru.kseniamedvedeva.arrayapp.service;
 
 public class ArrayFunctions {
 
+    public static int CountEvenNumbersInArray(int[] intArray) {
+        int countEvenNumb = 0;
+        for (int i : intArray) {
+            if (intArray[i] % 2 == 0) {
+                countEvenNumb++;
+            }
+        }
+        return countEvenNumb;
+    }
 
+    public static int CountOddNumbersInArray(int[] intArray) {
+        int countOddNumb = 0;
+        for (int i : intArray) {
+            if (intArray[i] % 2 != 0) {
+                countOddNumb++;
+            }
+        }
+        return countOddNumb;
+    }
+
+    public static int SumNumbersInArray(int[] intArray) {
+        int sum = 0;
+        for (int i : intArray) {
+            sum += intArray[i];
+        }
+        return sum;
+    }
+
+    public static int FindZeroInArray(int[] intArray) {
+        int zeroNum = 0;
+        for (int i : intArray) {
+            if (intArray[i] == 0) {
+                zeroNum++;
+            }
+        }
+        return zeroNum;
+    }
 }
