815b3f6f6e39866573e8cd368ee048aa01176177
Ksenia <ksenia.medvedeva.1993@gmail.com>
2024-03-15 18:12:44 +0500

    fixed errors

--- src/main/java/ru/kseniamedvedeva/arrayapp/_main/_Main.java
+++ src/main/java/ru/kseniamedvedeva/arrayapp/_main/_Main.java
@@ -12,22 +12,16 @@ public class _Main {
             intArray[i] = (int) (Math.random() * 100);
         }
         System.out.println(Arrays.toString(intArray));
-
         int countEvenNumb = ArrayFunctions.CountEvenNumbersInArray(intArray);
         System.out.println(countEvenNumb + " numbers are even in the array");
-
         int countOddNumb = ArrayFunctions.CountOddNumbersInArray(intArray);
         System.out.println(countOddNumb + " numbers are odd in the array");
-
         int countPrimeNum = ArrayFunctions.CountPrimeNumbersInArray(intArray);
         System.out.println(countPrimeNum + " numbers are prime in the array");
-
         int sum = ArrayFunctions.SumNumbersInArray(intArray);
         System.out.println("Summa all numbers in the array = " + sum);
-
         int difEvenOddNum = ArrayFunctions.DifferenceBetweenEvenAndOddIndexes(intArray);
         System.out.println("Difference between even and odd indexes = " + difEvenOddNum);
-
         int zeroNum = ArrayFunctions.FindZeroInArray(intArray);
         System.out.println(zeroNum + " zero in the array");
     }
